66848a1b21c856e341cc3cded53755b182dd2b80
Antonio Kobashikawa <akobashikawa@gmail.com>
2025-04-01 13:59:41 -0500

    fix: agrega ids

--- public/vue/e2e.test.js
+++ public/vue/e2e.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('Debe ingresar un nombre y mostrar el saludo', async ({ page }) => {
     // Navegar a la página
-    await page.goto('http://localhost:3000/vanilla/index.html');
+    await page.goto('http://localhost:3000/vue/index.html');
 
     // Seleccionar el campo de texto y escribir "Juan"
     const nameInput = await page.locator('#nameInput');
