778d46e2d072df6e0147e1c7a3a2341aed4d997d
Antonio Kobashikawa <akobashikawa@gmail.com>
2025-04-02 11:04:36 -0500

    feat: sendMessage base

--- public/angular/src/app/app.component.ts
+++ public/angular/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+
+  imports: [FormsModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular';
+  message: string = '';
+
+  sendMessage() {
+    console.log(this.message);
+  }
 }
